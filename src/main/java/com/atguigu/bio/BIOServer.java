219d0ae76bf4e93bcebf23fdb651423cd5356463
Nagisaki <454977355@qq.com>
2020-10-19 22:40:29 +0800

    尚硅谷netty 开始

---
+++ src/main/java/com/atguigu/bio/BIOServer.java
@@ -0,0 +1,13 @@
+package com.atguigu.bio;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BIOServer {
+
+    public static void main(String[] args) {
+        ExecutorService newCachedThreadPool = Executors.newCachedThreadPool();
+        
+    }
+}
